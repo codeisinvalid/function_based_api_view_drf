ea09857e9332d96ab5160f264d486e4d19331022
sooj <soojksh@gmail.com>
2024-04-15 20:05:04 +0530

    Student model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'grade', 'roll','city')
+    list_display_links=['name']
 
 # Register your models here.
+admin.site.register(Student)
--- api/views.py
+++ api/views.py
@@ -3,7 +3,25 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
-@api_view()
+# @api_view()
+# def student_create(request):
+#     return Response({'msg':'Hello, World'})
+    
+# @api_view(['GET'])
+# def student_create(request):
+#     return Response({'msg':'Hello, World'})
+    
+# @api_view(['POST'])
+# def student_create(request):
+#     if request.method=='POST':
+#         print(request.data)
+#         return Response({'msg':'Hello, World! This is post request'})
+    
+@api_view(['GET','POST'])
 def student_create(request):
-    return Response({'msg':'Hello, World'})
+    if request.method == 'GET':
+        return Response({'msg':'Hello, world! This is get request.'})
+    if request.method=='POST':
+        print(request.data)
+        return Response({'msg':'Hello, World! This is post request', 'data':request.data})
     
